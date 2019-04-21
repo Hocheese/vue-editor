23b7c0fe38d1a1d466cf89b1b4651a69a749318c
王贺青 <2653591247@qq.com>
2019-04-21 16:29:10 +0800

    核心技术攻关-可视化与代码I
    在线编辑器第一个核心技术已经突破！朕已经初步实现可视化编辑器与代码编辑器的数据同步。

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="btn_ctr"></div>
+    <div id="ipt_area">
+      <div>
+        <Viewedit v-model="article"></Viewedit>
+      </div>
+      <div>
+        <textarea v-model="article" name="" id="" cols="30" rows="10"></textarea>
+        
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Viewedit from './components/Viewedit.vue'
 
 export default {
   name: 'app',
+  data(){
+    return{
+      article:"<a href='#'>hello</a>"
+    }
+  },
   components: {
-    HelloWorld
+    Viewedit
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  
 }
 </style>
---
+++ src/components/Viewedit.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="viewedit">
+    <div contenteditable="true" 
+    v-on:input="$emit('input', $event.target.innerHTML)"  v-html="value">{{value}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Viewedit',
+  props: {
+    value: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
